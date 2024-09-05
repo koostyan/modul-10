65cd36d263a705b8f52720564cb0874ac14b84fe
koostyan <165835474+koostyan@users.noreply.github.com>
2024-09-05 15:16:30 +0300

    Add files via upload

---
+++ potoki na klassah.py
@@ -0,0 +1,32 @@
+import threading
+import time
+
+
+class Knight(threading.Thread):
+    def __init__(self, name, power):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.power = power
+        self.enemies = 100
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+        days = 0
+        while self.enemies > 0:
+            self.enemies -= self.power
+            days += 1
+            time.sleep(1)
+            print(f"{self.name} сражается {days} день(ей)..., осталось {self.enemies} воинов.")
+        print(f"{self.name} одержал победу спустя {days} дней!")
+
+
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+
+print("Битвы окончены!")
